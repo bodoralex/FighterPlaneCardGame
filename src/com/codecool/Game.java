a33e153add529e40e88e9069853424faa7500f26
bodoralex <bodaleaa@gmail.com>
2017-02-28 14:40:28 +0100

    Game.deal() implemented deck fixed

--- src/com/codecool/Game.java
+++ src/com/codecool/Game.java
@@ -1,30 +1,52 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Game {
 
-	public void getplayers() {
-		// TODO Auto-generated method stub
-		
+	private ArrayList<PlayCapable> players = new ArrayList<PlayCapable>();
+
+	private Printer printer;
+
+	public ArrayList<PlayCapable> getPlayers() {
+		return players;
 	}
 
 	public Player round() {
 		// TODO Auto-generated method stub
-		
-//		attack 
-//			card 0/1/2/3
-//		defense
-//			boolean
+
+		// attack
+		// card 0/1/2/3
+		// defense
+		// boolean
 		return null;
 	}
 
-	public void deal() {
-		// TODO Auto-generated method stub
-		
+	public void deal(Deck deck) {
+		Scanner scanner = new Scanner(System.in);
+		printer.print("How many cards are you want to play with?");
+		Integer cardsNumber = new Integer(null);
+		while (cardsNumber == null) {
+			try {
+				String howMany = scanner.next();
+				cardsNumber = Integer.getInteger(howMany);
+			} catch (Exception e) {
+				printer.print("That is not an integer.");
+			}
+		}
+		scanner.close();
+		deck.handout(getPlayers(), cardsNumber);
+		printer.print("Cards are dealt.");
 	}
 
 	public void getCards(Card[] cards) {
-		
-		
+
+	}
+
+	public void setPrinter(Printer printer) {
+		this.printer = printer;
+
 	}
 
 }
--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -2,18 +2,23 @@ package com.codecool;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Deck deck = new Deck();
-        deck.listFiller();
-        System.out.println(deck.cards.get(1).getSpeed());
-        
-        
+		// Deck deck = new Deck();
+		// deck.listFiller();
+		// System.out.println(deck.cards.get(1).getSpeed());
+		//
+
 		Game game = new Game();
+		Deck deck = new Deck();
 		game.getplayers();
-		game.getCards(Card.values());
-		game.deal();
-		//game.play();
+		
+		deck.fillDeck(Card.values());
+		deck.shuffleCards();
+		//
+		game.deal(deck);
+		
+		// game.play();
 
-    }
+	}
 }
