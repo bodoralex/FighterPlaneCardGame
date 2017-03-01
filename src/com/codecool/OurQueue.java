76123bb8eb90470d1f903edf3b83eec0155b6d3e
bodoralex <bodaleaa@gmail.com>
2017-03-01 14:14:38 +0100

    ourqueue imlemented

---
+++ src/com/codecool/OurQueue.java
@@ -0,0 +1,40 @@
+package com.codecool;
+
+/**
+
+ * @author bodor
+ *
+ * @param <Card>
+ * FIFO
+ * queue
+ * no natural order
+ */
+
+public class OurQueue<Card> {
+	
+	Object[] deck = new Object[0];
+	
+	public void add(Card card){
+		Object[] newDeck = new Object[deck.length+1];
+		for (int i = 0; i < deck.length; i++) {
+			newDeck[i] = deck[i];
+		}
+		newDeck[newDeck.length-1] = card;
+		deck = newDeck;
+
+	}
+	public Card remove(){
+		Object[] newDeck = new Object[deck.length-1];
+		for (int i = 1; i < deck.length; i++) {
+			newDeck[i-1] = deck[i];
+		}
+		Card firstElem = (Card) deck[0];
+		deck = newDeck;
+		return firstElem;
+	}
+	public Card peek(){
+		return (Card) deck[0];
+		
+	}
+
+}
