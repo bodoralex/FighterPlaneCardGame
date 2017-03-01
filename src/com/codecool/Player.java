0f9bdea2d947352721d16dd50962650b7f86d1f3
regygu <ugyan.gergely@gmail.com>
2017-03-01 08:44:13 +0100

    player implemented

--- src/com/codecool/Player.java
+++ src/com/codecool/Player.java
@@ -1,31 +1,47 @@
 package com.codecool;
 
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Player implements PlayCapable {
 
-	@Override
-	public void attack() {
-		// TODO Auto-generated method stub
-		
-	}
+    Scanner scanner = new Scanner(System.in);
+    Printer printer = new Printer();
+    Queue<Card> hand = new LinkedList<>();
 
-	@Override
-	public void defense() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public int attack() {
+        Card topCard = hand.peek();
+        printer.print(topCard);
+        printer.print("Choose the attribute you want to use");
+        return getPlayerChoice();
+    }
 
-	@Override
-	public void setHand(Queue<Card> cards) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Card draw() {
+        return hand.remove();
+    }
 
-	@Override
-	public Queue<Card> getHand() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void setHand(Queue<Card> cards) {
+
+    }
+
+    @Override
+    public Queue<Card> getHand() {
+        return null;
+    }
+
+    public int getPlayerChoice() {
+        boolean goodInput = false;
+        while (!goodInput) {
+            int playerChoice = scanner.nextInt();
+            if (playerChoice == 1 || playerChoice == 2 || playerChoice == 3 || playerChoice == 4) {
+                return playerChoice;
+            }
+        }
+        return 0;
+    }
 
 }
