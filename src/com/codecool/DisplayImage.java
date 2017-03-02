d02ff28cca889aa09e5c0493a394eff694a4706a
Marcell Lovácsi <m.lovacsi@gmail.com>
2017-03-02 11:31:02 +0100

    images get resized correctly

--- src/com/codecool/DisplayImage.java
+++ src/com/codecool/DisplayImage.java
@@ -1,5 +1,6 @@
 package com.codecool;
-import java.awt.FlowLayout;
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,35 +8,45 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 /**
  * Created by Lovi on 2017. 03. 02. @ 0:43.
  */
-public class DisplayImage extends Thread{
-		Card cardGot;
-	public DisplayImage(Card c)  throws IOException
-	{
-		cardGot = c;
-	}
+public class DisplayImage extends Thread {
+    Card cardGot;
 
-	@Override
-	public void run() {
-		try {
-			BufferedImage img=ImageIO.read(new File("src//"+cardGot.getID()));
-		ImageIcon icon=new ImageIcon(img);
+    public DisplayImage(Card c) throws IOException {
+        cardGot = c;
+    }
+    public static BufferedImage resizeImage(BufferedImage originalImage, int type){
+        BufferedImage resizedImage = new BufferedImage(512, 256, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, 512, 256, null);
+        g.dispose();
 
-		JFrame frame=new JFrame();
-		JLabel lbl=new JLabel();
-		frame.setLayout(new FlowLayout());
-		frame.setSize(icon.getIconWidth(),icon.getIconHeight());
-		lbl.setIcon(icon);
-		frame.add(lbl);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Thread.sleep(3000);
-		frame.setVisible(false);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+        return resizedImage;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedImage originalImage = ImageIO.read(new File("src//" + cardGot.getID()));
+            int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+            BufferedImage resizedImage = resizeImage(originalImage,type);
+            ImageIcon icon = new ImageIcon(resizedImage);
+            JFrame frame = new JFrame();
+            JLabel lbl = new JLabel();
+            frame.setLayout(new FlowLayout());
+            frame.setSize(512,256);
+            lbl.setIcon(icon);
+            frame.add(lbl);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            Thread.sleep(3000);
+            frame.setVisible(false);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
